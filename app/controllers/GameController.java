1e8ca6f34b106d70ab86046e7061f81c4ce9f5bb
Louis <lrflrf@sbcglobal.net>
2019-02-27 16:53:39 -0600

    Start Silver

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,28 +1,74 @@
 package controllers;
 
+import play.Logger;
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
+import play.mvc.Http;
 import play.mvc.Result;
+
+import javax.inject.Inject;
 
 public class GameController extends Controller
 {
+    private FormFactory formFactory;
+    private static final Logger.ALogger logger = Logger.of(GameController.class);
+
+    private static final String PLAYER_NAME_SESSION_KEY = "PLAYER_NAME";
+    private static final String CREW_COUNT_SESSION_KEY = "CREW_COUNT";
+    private static final int STARTING_CREW_COUNT = 10;
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+    }
+
     public Result getWelcome()
     {
+        logger.debug("getWelcome");
         return ok(views.html.welcome.render());
     }
 
-    public Result postStart()
+    public Result postStart(Http.Request request)
+    {
+        DynamicForm form = formFactory.form().bindFromRequest(request);
+        String playerName = form.get("playerName");
+
+        return ok(views.html.start.render())
+                 .addingToSession(request, PLAYER_NAME_SESSION_KEY, playerName)
+                 .addingToSession(request, CREW_COUNT_SESSION_KEY, String.valueOf(STARTING_CREW_COUNT));
+    }
+
+    public Result postRestart()
     {
         return ok(views.html.start.render());
     }
 
-    public Result postEastFromEngland()
+    public Result postEastFromEngland(Http.Request request)
     {
-        return ok(views.html.eastfromengland.render());
+        String crewCount = getDecrementedCrewCount(request);
+
+        return ok(views.html.eastfromengland.render(crewCount)).addingToSession(request, CREW_COUNT_SESSION_KEY, crewCount);
     }
 
-    public Result postNorthEnd()
+    private String getDecrementedCrewCount(Http.Request request)
     {
-        return ok(views.html.northend.render());
+        String crewCountAsString = request.session().getOptional(CREW_COUNT_SESSION_KEY).orElse(String.valueOf(STARTING_CREW_COUNT));
+        int crewCount = Integer.parseInt(crewCountAsString);
+        crewCount--;
+        return String.valueOf(crewCount);
+    }
+
+    public Result postNorthEnd(Http.Request request)
+    {
+        String playerName = request.session().getOptional(PLAYER_NAME_SESSION_KEY).orElse("Unknown");
+        return ok(views.html.northend.render(playerName));
+    }
+
+    public Result postNorthFromEngland()
+    {
+        return ok(views.html.northfromengland.render());
     }
 
     public Result postWestFromEngland()
@@ -49,4 +95,5 @@ public class GameController extends Controller
     {
         return ok(views.html.kittens.render());
     }
+
 }
